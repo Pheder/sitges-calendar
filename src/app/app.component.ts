558d8dbd9f7c1996f3cd5e2d4f9a81b1b9180ea6
Joan <joan.pedreno@fhios.es>
2018-09-18 17:18:53 +0200

    initial upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,128 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
+
+import {
+  HttpClient
+} from '@angular/common/http';
+
+import {
+  isSameDay,
+  isSameMonth
+} from 'date-fns';
+import {
+  Subject
+} from 'rxjs';
+import {
+  NgbAccordion
+} from '@ng-bootstrap/ng-bootstrap';
+import {
+  CalendarEvent,
+  CalendarEventAction,
+  CalendarView
+} from 'angular-calendar';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'sitges-calendar';
+
+export class AppComponent  {
+  view: CalendarView = CalendarView.Month;
+
+  CalendarView = CalendarView;
+
+  viewDate: Date = new Date(2018, 9, 1);
+
+  actions: CalendarEventAction[] = [{
+      label: '<i class="fa fa-fw fa-pencil"></i>',
+      onClick: ({
+        event
+      }: {
+        event: CalendarEvent
+      }): void => {}
+    },
+    {
+      label: '<i class="fa fa-fw fa-times"></i>',
+      onClick: ({
+        event
+      }: {
+        event: CalendarEvent
+      }): void => {
+        this.events = this.events.filter(iEvent => iEvent !== event);
+      }
+    }
+  ];
+
+  refresh: Subject < any > = new Subject();
+
+  events: CalendarEvent[] = [];
+  movies: any;
+  title: string = "";
+
+  activeDayIsOpen: boolean = false;
+
+  constructor(private http: HttpClient) {
+
+    let movies = localStorage.getItem('movies');
+    if (movies) {
+      this.movies = JSON.parse(movies);
+      this.movies.forEach(movie => {
+        movie.actions = this.actions,
+          movie.start = new Date(movie.start);
+        movie.end = new Date(movie.end);
+        movie.day = movie.start.getDate();
+        if (movie.active) {
+          this.events.push(movie);
+        }
+      });
+    } else {
+      http.get('assets/movies.json').subscribe(data => {
+        data['movies'].forEach(movie => {
+          movie.actions = this.actions,
+            movie.start = new Date(movie.start);
+          movie.end = new Date(movie.end);
+          movie.day = movie.start.getDate();
+        });
+        this.movies = data['movies'];
+      });
+    }
+
+
+  }
+
+  dayClicked({
+    date,
+    events
+  }: {
+    date: Date;events: CalendarEvent[]
+  }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      this.viewDate = date;
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+      }
+    }
+  }
+
+
+  toggle(session): void {
+    if (session.active) {
+      session.active = false;
+      this.events.splice(this.events.indexOf(session), 1);
+    } else {
+      this.events.push(session);
+      session.active = true;
+    }
+    this.refresh.next();
+    localStorage.setItem('movies', JSON.stringify(this.movies));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter} from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { FormsModule } from '@angular/forms';
+import {NgbAccordionModule} from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { GroupByPipe } from './groupby';
+import { SearchPipe } from './search';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupByPipe,
+    SearchPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    NgbAccordionModule,
+    FormsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
   ],
+  exports: [SearchPipe],
   providers: [],
   bootstrap: [AppComponent]
 })
